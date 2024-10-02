00e96659581bb11d82900e8edd2458c8224f7642
Antonio <88379747+sobrino29@users.noreply.github.com>
2024-10-02 11:20:23 +0200

    Add files via upload

---
+++ src/main/java/antonio/jenkins/archivo/ArchivoApplication.java
@@ -0,0 +1,13 @@
+package antonio.jenkins.archivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/antonio/jenkins/archivo/ArchivoApplicationTests.java
@@ -0,0 +1,13 @@
+package antonio.jenkins.archivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
